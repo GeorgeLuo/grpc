2836d0b533d331cdd3e5202ff5da475634b9f39f
MyMac <mymac@MacBook-Pro-7.fios-router.home>
2019-11-23 19:55:53 -0500

    start config work

--- main.go
+++ main.go
@@ -3,19 +3,63 @@ package main
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/json"
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/GeorgeLuo/grpc/core"
 	"github.com/GeorgeLuo/grpc/jobs"
+	"github.com/GeorgeLuo/grpc/utils"
 	"github.com/gorilla/mux"
 )
 
 // TODO: add a handler to eventstream output of process
 
 func main() {
+	var config utils.ServerConfig
+	var err error
+
+	if len(os.Args) < 2 {
+		log.Println("operation not provided")
+		os.Exit(1)
+	}
+
+	startServer := flag.NewFlagSet("start", flag.ExitOnError)
+	configFile := startServer.String("conf", "grpc_server_conf.json",
+		"start configurations")
+
+	switch os.Args[1] {
+	case "start":
+		err = startServer.Parse(os.Args[2:])
+	default:
+		fmt.Println("invalid command")
+		os.Exit(1)
+	}
+
+	if err != nil {
+		fmt.Printf("error parsing request:\n %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	log.Printf("loading configurations from: %s", *configFile)
+	config, err = utils.LoadServerConfiguration(*configFile)
+	if err != nil {
+		log.Printf("config file not not found, using default configurations")
+	}
+
+	prettyConf, err := json.MarshalIndent(config, "", "  ")
+	if err != nil {
+		fmt.Println("error printing configs: ", err)
+		os.Exit(1)
+	}
+
+	log.Println(string(prettyConf))
 	log.Println("setting up handlers ...")
+
 	router := mux.NewRouter()
 	router.HandleFunc("/status", core.StatusHandler).
 		Methods("POST")
@@ -33,7 +77,7 @@ func main() {
 		Methods("POST")
 
 	// Create a CA certificate pool and add cert.pem to it
-	caCert, err := ioutil.ReadFile("cert.pem")
+	caCert, err := ioutil.ReadFile(config.CaCertFile)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -50,10 +94,10 @@ func main() {
 	// Create a Server instance to listen on port 8443 with the TLS config
 	server := &http.Server{
 		Handler:   router,
-		Addr:      "0.0.0.0:8443",
+		Addr:      "0.0.0.0:" + config.Port,
 		TLSConfig: tlsConfig,
 	}
 
 	// Listen to HTTPS connections with the server certificate and wait
-	log.Fatal(server.ListenAndServeTLS("cert.pem", "key.pem"))
+	log.Fatal(server.ListenAndServeTLS(config.CertFile, config.KeyFile))
 }
